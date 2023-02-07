baad9815036fe3da2308aece1aae0fb141e7a5c5
joolimoon <bznsym@gmail.com>
2023-02-07 19:47:07 +0200

    LECTURE 15 1st coding challenge

---
+++ 01-Fundamentals-Part-1/challenges.js
@@ -0,0 +1,23 @@
+// Coding Challenge #1
+// Data 1
+// const johnsMass = 92;
+// const johnsHeight = 1.95;
+
+// const marksMass = 78;
+// const marksHeight = 1.69;
+
+// Data 2
+johnsMass = 85;
+johnsHeight = 1.76;
+
+marksMass = 95;
+marksHeight = 1.88;
+
+const marksBMI = marksMass / (marksHeight * marksHeight);
+const johnsBMI = johnsMass / johnsHeight ** 2;
+
+const markHigherBMI = marksBMI > johnsBMI;
+
+console.log(marksBMI, johnsBMI, markHigherBMI);
+
+// Coding Challenge #2
