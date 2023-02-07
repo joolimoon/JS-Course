f83ca374967fc371d6bd6119161587410d998e56
joolimoon <bznsym@gmail.com>
2023-02-07 22:37:05 +0200

    to lecture 24

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -145,7 +145,7 @@ console.log(`string with
 multible 
 lines`);
 
-*/
+
 
 // 888888888888888888
 // Taking Decisions: if / else Statements
@@ -168,3 +168,106 @@ if (birthYear <= 2000) {
   century = 21;
 }
 console.log(century);
+
+
+
+// 888888888888888888
+// Type Conversion and Coercion
+// 888888888888888888
+
+// type convertion
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number("jonas"));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+// type coertion
+console.log("I'am " + 23 + " years old");
+console.log("23" - "10" - 3);
+console.log("23" / "2");
+
+let n = "1" + 1; //11
+n = n - 1;
+console.log(n);
+
+
+// 888888888888888888
+// Truthy and Falsy Values
+// 888888888888888888
+
+// 5 falsy values: 0 , undefined , null , NaN , ""
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean("jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
+
+const money = 100;
+if (money) {
+  console.log("Don't spend it all :)");
+} else {
+  console.log("you should get a job!");
+}
+
+let height = 0;
+if (height) {
+  console.log("YAY! Height is defid");
+} else {
+  console.log("Height is UNDEFINED");
+}
+
+
+// 888888888888888888
+// Equality Operators: == vs. ===
+// 888888888888888888
+const age = "18";
+if (age === 18) console.log("You just become an adult :D (strict)");
+
+if (age == 18) console.log("You just become an adult :D (loose)");
+
+const favourite = prompt("what is your favourite number?");
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  // 22 === 23 -> FALSE
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a gool number");
+} else if (favourite === 9) {
+  console.log("9 is also a gool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) console.log("why not 23?");
+*/
+
+// 888888888888888888
+// Logical Operators
+// 888888888888888888
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+// if (hasDriversLicense && hasGoodVision) {
+//   console.log("Sarah is able to drive");
+// } else {
+//   console.log("Some one else should drive...");
+// }
+
+const isTired = false; // C
+if (hasDriversLicense && hasGoodVision && !isTired) {
+  console.log("Sarah is able to drive");
+} else {
+  console.log("Some one else should drive...");
+}
