76aac32b7064e37f1698278026c4a89ea63108f9
joolimoon <bznsym@gmail.com>
2023-02-07 17:45:49 +0200

    add assignmets file and solve the 1st assignment

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 // Linking a JavaScript File
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
@@ -22,3 +23,5 @@ let job1 = "Programmer";
 let job2 = "Teacher";
 
 console.log(myFirstJob);
+*/
+// Data Types
