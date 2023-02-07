5fb01cb0fbee7b7ef0d8449403681e17f6cd1dff
joolimoon <bznsym@gmail.com>
2023-02-07 23:29:45 +0200

    3ed challenge

--- 01-Fundamentals-Part-1/challenges.js
+++ 01-Fundamentals-Part-1/challenges.js
@@ -1,5 +1,7 @@
 /*
+888888888888888888888888
 // Coding Challenge #1
+888888888888888888888888
 // Data 1
 // let johnsMass = 92;
 // let johnsHeight = 1.95;
@@ -20,9 +22,10 @@ const johnsBMI = johnsMass / johnsHeight ** 2;
 const markHigherBMI = marksBMI > johnsBMI;
 
 console.log(marksBMI, johnsBMI, markHigherBMI);
-*/
-// Coding Challenge #2
 
+8888888888888888888888888
+// Coding Challenge #2
+8888888888888888888888888
 let johnsMass = 92;
 let johnsHeight = 1.95;
 
@@ -44,3 +47,117 @@ if (marksBMI > johnsBMI) {
 } else {
   console.log(`John's BMI ${johnsBMI}  is higher than Mark's ${marksBMI}!`);
 }
+
+
+8888888888888888888888888
+// Coding Challenge #2
+8888888888888888888888888
+
+let dolphinsAverageScore = (96 + 108 + 89) / 3;
+let koalasAverageScore = (88 + 91 + 110) / 3;
+console.log(dolphinsAverageScore, koalasAverageScore);
+
+if (dolphinsAverageScore > koalasAverageScore)
+  console.log("Dolphins wins the trophy");
+
+if (koalasAverageScore > dolphinsAverageScore)
+  console.log("Koalas wins the trophy");
+
+if (koalasAverageScore === dolphinsAverageScore) console.log("it's a Draw 🥱");
+
+// Bonus 1
+
+dolphinsAverageScore = (97 + 112 + 101) / 3;
+koalasAverageScore = (109 + 95 + 106) / 3;
+const minimumScore = 100;
+console.log(dolphinsAverageScore, koalasAverageScore);
+if (
+  dolphinsAverageScore > koalasAverageScore &&
+  dolphinsAverageScore > minimumScore
+) {
+  console.log("Dolphins wins the trophy");
+} else if (
+  koalasAverageScore > dolphinsAverageScore &&
+  koalasAverageScore > minimumScore
+) {
+  console.log("Koalas wins the trophy");
+} else if (dolphinsAverageScore === koalasAverageScore) {
+  console.log("it's a Draw 🥱");
+}
+
+// Bonus 2
+
+dolphinsAverageScore = (97 + 112 + 101) / 3;
+koalasAverageScore = (109 + 95 + 123) / 3;
+console.log(dolphinsAverageScore, koalasAverageScore);
+
+if (
+  dolphinsAverageScore > koalasAverageScore &&
+  dolphinsAverageScore > minimumScore
+) {
+  console.log("Dolphins wins the trophy");
+} else if (
+  koalasAverageScore > dolphinsAverageScore &&
+  koalasAverageScore > minimumScore
+) {
+  console.log("Koalas wins the trophy");
+} else if (
+  dolphinsAverageScore === koalasAverageScore &&
+  (dolphinsAverageScore === koalasAverageScore) === minimumScore
+) {
+  console.log("it's a Draw 🥱");
+} else {
+  console.log("Sadly no team wins 😪");
+}
+
+
+8888888888888888888888888
+// Coding Challenge #2
+8888888888888888888888888
+
+
+let dolphinsAverageScore = (96 + 108 + 89) / 3;
+let koalasAverageScore = (88 + 91 + 110) / 3;
+let score = dolphinsAverageScore - koalasAverageScore;
+console.log(dolphinsAverageScore, koalasAverageScore, score);
+
+if (score > 1) {
+  console.log("Dolphins wins the trophy");
+} else if (score < 1) {
+  console.log("Koalas wins the trophy");
+}
+
+// BONAS 1
+dolphinsAverageScore = (97 + 112 + 101) / 3;
+koalasAverageScore = (109 + 95 + 106) / 3;
+score = dolphinsAverageScore - koalasAverageScore;
+const minimumScore = 100;
+
+console.log(dolphinsAverageScore, koalasAverageScore, score);
+
+if (score > 1 && dolphinsAverageScore >= minimumScore) {
+  console.log("Dolphins wins the trophy");
+} else if (score < 1 && koalasAverageScore >= minimumScore) {
+  console.log("Koalas wins the trophy");
+} else {
+  console.log("it's a Draw 🥱");
+}
+
+// BONAS 2
+
+dolphinsAverageScore = (97 + 112 + 101) / 3;
+koalasAverageScore = (109 + 95 + 123) / 3;
+score = dolphinsAverageScore - koalasAverageScore;
+console.log(dolphinsAverageScore, koalasAverageScore, score);
+
+if (score > 1 && dolphinsAverageScore >= minimumScore) {
+  console.log("Dolphins wins the trophy");
+} else if (score < 1 && koalasAverageScore >= minimumScore) {
+  console.log("Koalas wins the trophy");
+} else if (score === 1 && koalasAverageScore <= minimumScore) {
+  console.log("it's a Draw 🥱");
+} else {
+  console.log("Sadly no team wins 😪");
+}
+
+*/
